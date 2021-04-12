8f1b31c9f6567ec6756c4b428c964f39b2a7c184
nvargas6 <nelsonjvargas.00@gmail.com>
2021-04-12 13:59:19 -0400

    retrieved matchlist sparked_prophet total result 168

--- main.py
+++ main.py
@@ -1,37 +1,39 @@
 import json
 import os
-from pathlib import Path
-
 import requests
+import pandas as pd
+from pathlib import Path
+from riotwatcher import LolWatcher, ApiError
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 def absolute_path_test():
     path = os.path.dirname(__file__)
-    auth_path = Path(path + "/data/api_key.json")
+    auth_path = Path(path + '/data/api_key.json')
     with open(auth_path) as file:
+        key = json.load(file)
+        print(key)
+        return key['authkey']
 
-        #data = json.load(file)
-        authkey = json.load(file)
-        print(authkey)
-        return authkey["authkey"]
-    #auth_path.read_text())
 
-def api_call():
-    api_base = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Doublelift?api_key=RGAPI-YOUR-API-KEY"
-
+# globals
+authkey = absolute_path_test()
+api_key_url = '?api_key='
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-    URL = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/sparked_prophet?api_key=" \
-          + absolute_path_test()
+    URL = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/sparked_prophet?api_key=' \
+          + authkey
 
     print(URL)
     print(absolute_path_test())
+    request = requests.get(URL)
+    print(request)
+    data = request.json()
+    print(data)
+
+    matchlist_url = 'https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/'
+    URL = matchlist_url + 'ldmTvN5T3T-3GLzJoZ-OggPOiFuYI-aWVkaVFs3QtBniq3wE-NBsl3Dp' + api_key_url + authkey
+    print('****')
+    print(URL)
     request = requests.get(URL)
     print(request)
     data = request.json()
