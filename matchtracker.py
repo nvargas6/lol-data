ce5a6f6b8e4ae7dee3a39d78738920a6b0e01da8
nvargas6 <nelsonjvargas.00@gmail.com>
2021-04-12 14:31:46 -0400

    creating setup tracker and matchtracker

---
+++ matchtracker.py
@@ -0,0 +1,7 @@
+#imports
+class MatchTracker:
+
+    def __init__(self, summoner_id, api_key):
+        self.summoner_id = summoner_id
+        self.api_key = api_key
+
---
+++ setuptracker.py
@@ -0,0 +1,10 @@
+#imports
+
+def extract_summoner_info():
+    print('*****')
+
+
+class SetupTracker:
+    def __init__(self, summoner_id, api_key):
+        self.summoner_id = summoner_id
+        self.api_key = api_key
